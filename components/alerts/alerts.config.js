b2ef040a8d43331641d80024e5de7070c107dfdc
dlousa92 <dlousa92@gmail.com>
2019-03-22 14:03:31 -0400

    different ways of getting and displaying data

---
+++ components/alerts/alerts.config.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const faker = require('faker')
+const sampleDataCount = 15
+const userData = []
+
+for (let i = 0; i < sampleDataCount; i++) {
+    userData.push({
+        name: faker.name.findName(),
+        job: faker.name.jobTitle(),
+        department: faker.commerce.department(),
+        phone: faker.phone.phoneNumber(),
+        description: 'They are ' + faker.hacker.adjective() + '.'
+    })
+}
+console.log(userData)
+module.exports = {
+    context: {
+        users: userData
+    }
+}
